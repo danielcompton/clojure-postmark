a1c07ee901456fd7d83a838b58cc66d520ec2bf9
Bob Nadler <bnadlerjr@gmail.com>
2015-01-27 21:44:39 -0500

    Update cheshire & clj-http dependencies.

--- project.clj
+++ project.clj
@@ -2,5 +2,10 @@
   :description "Clojure bindings for http://postmarkapp.com/"
   :exclusions  [org.clojure/clojure]
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [cheshire "2.1.0"]
-                 [clj-http "0.3.0"]])
+                 [cheshire "5.4.0"]
+                 [clj-http "0.7.8"]])
+  :plugins  [[lein-ancient "0.6.1"]
+             [lein-kibit "0.0.8"]
+             [jonase/eastwood "0.2.1"]]
+  :aliases {"lint" ["do" ["ancient"] ["kibit"] ["eastwood"]]})
+>>>>>>> 68dba29... Troubleshoot clj-http by downgrading library.
