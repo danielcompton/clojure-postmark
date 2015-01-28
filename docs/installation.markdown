1dd5341d3bab2cdeff3b3fc73be229ed8db5bb3d
Bob Nadler <bnadlerjr@gmail.com>
2015-01-27 19:14:34 -0500

    Don't pin Clojure version.

--- docs/installation.markdown
+++ docs/installation.markdown
@@ -5,4 +5,4 @@ To get started just slap this in your `project.clj` `:dependencies`:
 
     [postmark "1.1.0"]
 
-`clojure-postmark` works with Clojure 1.2 and 1.3.
+`clojure-postmark` works with Clojure 1.6.0.
--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject postmark "1.2.0-SNAPSHOT"
   :description "Clojure bindings for http://postmarkapp.com/"
-  :dependencies [[org.clojure/clojure "[1.2.1],[1.3.0]"]
+  :exclusions  [org.clojure/clojure]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [cheshire "2.1.0"]
                  [clj-http "0.3.0"]])
