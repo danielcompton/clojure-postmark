61b85f4546f5b5e2f5d0c406a2a9f0991667ae89
Bob Nadler <bnadlerjr@gmail.com>
2015-01-27 22:03:38 -0500

    Setup linting tools.

--- project.clj
+++ project.clj
@@ -3,9 +3,8 @@
   :exclusions  [org.clojure/clojure]
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [cheshire "5.4.0"]
-                 [clj-http "0.7.8"]])
+                 [clj-http "0.7.8"]]
   :plugins  [[lein-ancient "0.6.1"]
              [lein-kibit "0.0.8"]
              [jonase/eastwood "0.2.1"]]
   :aliases {"lint" ["do" ["ancient"] ["kibit"] ["eastwood"]]})
->>>>>>> 68dba29... Troubleshoot clj-http by downgrading library.
