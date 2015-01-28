963ba12f8df925521b727ead9b3f03410fa06323
Bob Nadler <bnadlerjr@gmail.com>
2015-01-27 21:51:48 -0500

    Update version to 1.2.0-SNAPSHOT.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject postmark "1.1.0"
+(defproject postmark "1.2.0-SNAPSHOT"
   :description "Clojure bindings for http://postmarkapp.com/"
   :dependencies [[org.clojure/clojure "[1.2.1],[1.3.0]"]
                  [cheshire "2.1.0"]
