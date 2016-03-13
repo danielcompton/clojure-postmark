7a51662929b56d1e46de2b2529d36c084b16b46e
Edipo L Federle <edipofederle@gmail.com>
2016-03-13 20:46:27 -0300

    Update dependencies and add suport to templates

--- project.clj
+++ project.clj
@@ -6,10 +6,10 @@
   :license {:name "MIT"
             :url "https://github.com/sjl/clojure-postmark/blob/master/LICENSE.markdown"}
   :exclusions  [org.clojure/clojure]
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [cheshire "5.4.0"]
-                 [clj-http "0.7.8"]]
-  :plugins  [[lein-ancient "0.6.1"]
-             [lein-kibit "0.0.8"]
-             [jonase/eastwood "0.2.1"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [cheshire "5.5.0"]
+                 [clj-http "2.1.0"]]
+  :plugins  [[lein-ancient "0.6.8"]
+             [lein-kibit "0.1.2"]
+             [jonase/eastwood "0.2.3"]]
   :aliases {"lint" ["do" ["ancient"] ["kibit"] ["eastwood"]]})
--- src/postmark/core.clj
+++ src/postmark/core.clj
@@ -11,8 +11,14 @@
   [mail]
   (generate-string (into {} (filter second mail))))
 
+(defn- postmark-url [mail]
+  "Return the correct postmark API url."
+  (if (contains? mail "TemplateId")
+    "http://api.postmarkapp.com/email/withTemplate"
+    "http://api.postmarkapp.com/email"))
+
 (defn- send-to-postmark [api-key mail]
-  (let [resp (client/post "http://api.postmarkapp.com/email"
+  (let [resp (client/post (postmark-url mail)
                           {:body (mail-to-json mail)
                            :headers {"X-Postmark-Server-Token" api-key}
                            :content-type :json
@@ -52,6 +58,12 @@
                              "HtmlBody" html
                              "ReplyTo" reply-to}))
 
+(defn- mail-template
+  [api-key from {:keys [:to, :template_id, :template_model]}]
+  (send-to-postmark api-key {"From" from
+                                   "To" (get-to-string to)
+                                   "TemplateId" template_id
+                                   "TemplateModel" template_model}))
 
 (defn postmark [api-key from]
   (partial mail api-key from))
